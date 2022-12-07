b7e91a3a561b5beddb2a059350c8291334f37409
giorgip62 <giorgiop62@gmail.com>
2022-12-07 17:57:28 +0100

    parte base

--- class/movie.php
+++ class/movie.php
@@ -8,21 +8,19 @@ class Movie
   public $img;
 
 
+  // Dependecy Injection
   public function __construct($_regista, $_titolo, $_genere)
   {
    $this ->regista = $_regista;
    $this ->titolo = $_titolo;
-   $this ->genere = $_genere;
-  
+   $this ->genere = $_genere;  
   }
 
   public function setImg($_img)
   {
     $this->img = $_img;
-    
+
   }
-
-
 }
 
 
--- index.php
+++ index.php
@@ -13,3 +13,26 @@ $movie2 ->setImg('https://pad.mymovies.it/filmclub/2012/05/105/locandina.jpg');
 var_dump($movie1);
 var_dump(($movie2));
 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.min.js" integrity="sha512-1/RvZTcCDEUjY/CypiMz+iqqtaoQfAITmNSJY17Myp4Ms5mdxPS5UV7iOfdZoxcGhzFbOm6sntTKJppjvuhg4g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <link rel="stylesheet" href="./css/style.css">
+  <title>Movie</title>
+</head>
+<body>
+  <header>
+    <h1>I film preferiti di Giorgio </h1>
+
+  </header>
+
+
+
+  
+</body>
+</html>
